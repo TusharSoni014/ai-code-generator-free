88614ca91286ded6eeea6fbb317dc3c97a3a2d6a
Tushar Soni <71227235+TusharSoni014@users.noreply.github.com>
2024-03-24 15:28:20 +0530

    cors fix trial 2

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,8 +5,14 @@ config();
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: process.env.FRONTEND_URL }));
-console.log("ENV URL: ", process.env.FRONTEND_URL);
+app.use(
+  cors({
+    origin: [
+      "https://ai-code-generator-free.vercel.app",
+      "http://localhost:5173",
+    ],
+  })
+);
 
 app.post("/", async (req, res) => {
   const { prompt } = req.body;
